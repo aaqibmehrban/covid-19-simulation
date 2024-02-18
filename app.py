ee29cbeaecb606e7f64216cbc96d3ad61933b26b
Aaqib Mehrban <46885712+aaqibmehrban@users.noreply.github.com>
2024-02-18 10:26:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run_server(host='0.0.0.0', port=port)
     
 

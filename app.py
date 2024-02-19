22cb4e190505660bd4a3f0a906d10d4c161135fb
Aaqib Mehrban <46885712+aaqibmehrban@users.noreply.github.com>
2024-02-19 19:04:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
+import os
+
 from dash import Dash, html, dcc
-import plotly.express as px
-from simulation import *
 from updated_graphs import*
-
-
-
-
-
 
 
 
@@ -18,38 +13,25 @@ fig5=sir.verify_sir_model_plotly(flow_matrix_2020, nodes_2020, city_properties_2
 
 
 app = Dash(__name__)
-server = app.server
+server=app.server
+
 
 app.layout = html.Div([
     html.Div([
         html.H1("Covid 19 Simulation", style={'textAlign': 'center'})
-    ], style={'background': 'linear-gradient(to right, lightgray, white)', 'padding': '10px', 'margin-bottom': '20px'}),
-    
-    # Body
+    ], style={'background': 'linear-gradient(to right, lightgray, white)', 'padding': '1vh', 'margin-bottom': '2vh'}),
+
     html.Div([
-        # Graphs container
-        html.Div([
-            # Column 1
-            html.Div([
-                html.Div([dcc.Graph(id='graph-1', figure=fig1)], style={'background-color': '#f8f9fa', 'margin': '10px', 'padding': '20px', 'border-radius': '5px'}),
-                html.Div([dcc.Graph(id='graph-3', figure=fig3)], style={'background-color': '#f8f9fa', 'margin': '10px', 'padding': '20px', 'border-radius': '5px'}),
-                html.Div([dcc.Graph(id='graph-5', figure=fig5)], style={'background-color': '#f8f9fa', 'margin': '10px', 'padding': '20px', 'border-radius': '5px'})
-            ], style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top'}),
-
-            # Column 2
-            html.Div([
-                html.Div([dcc.Graph(id='graph-2', figure=fig2)], style={'background-color': '#f8f9fa', 'margin': '10px', 'padding': '20px', 'border-radius': '5px'}),
-                html.Div([dcc.Graph(id='graph-4', figure=fig4)], style={'background-color': '#f8f9fa', 'margin': '10px', 'padding': '20px', 'border-radius': '5px'})
-            ], style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top'})
-        ], style={'display': 'flex', 'flexDirection': 'row'})
-    ], style={'padding': '20px'})
-], style={'background-color': 'white', 'fontFamily': 'Arial, sans-serif'})
-
-
+        dcc.Graph(id='graph-1', figure=fig1, style={'width': '90vw', 'height': '45vh'}),
+        dcc.Graph(id='graph-2', figure=fig2, style={'width': '90vw', 'height': '45vh'}),
+        dcc.Graph(id='graph-3', figure=fig3, style={'width': '90vw', 'height': '45vh'}),
+        dcc.Graph(id='graph-4', figure=fig4, style={'width': '90vw', 'height': '45vh'}),
+        dcc.Graph(id='graph-5', figure=fig5, style={'width': '90vw', 'height': '45vh'}),
+    ], style={'textAlign': 'center'})
+], style={'background-color': 'white', 'fontFamily': 'Arial, sans-serif', 'maxWidth': '100vw'})
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run_server(host='0.0.0.0', port=port)
-    
 
